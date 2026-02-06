f386ea00e4ceccf29d9492405564ca9c2b155635
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-02-06 22:26:52 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,14 @@
+export default function Home() {
+  return (
+    <div className="space-y-8">
+      <p>
+        Willkommen auf der AstroPlays Discord Bot Website!
+      </p>
+      <p>
+        Hier findest du Infos über den Bot, Befehle und alles andere.
+      </p>
+      <div className="h-[2000px]"></div> {/* Dummy Content zum Scrollen */}
+    </div>
+  );
+}
 
