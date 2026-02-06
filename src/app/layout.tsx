2f5f7dfbee3b3bc2913c3d42f2345081bc1cdede
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-02-06 22:26:39 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,22 @@
+import './styles/globals.css';
+import { ReactNode } from 'react';
+import FixedHeader from '../components/FixedHeader';
+
+export const metadata = {
+  title: 'AstroPlays Discord Bot',
+  description: 'Website für den Discord Bot AstroPlays',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="de">
+      <body>
+        <FixedHeader />
+        <main className="pt-[140px] px-4">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
 
