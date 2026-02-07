eba3d21ac7e2c21043a6861a0b661a9271da40dd
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-02-07 23:40:14 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import { ReactNode } from 'react';
 import FixedHeader from '../components/FixedHeader';
+import FixedFooter from '../components/FixedFooter';
 import { LanguageProvider } from '../context/LanguageContext';
 
 export const metadata = {
@@ -10,12 +11,18 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="de">
-      <body className="bg-gray-50">
-        {/* LanguageProvider kann weiterhin Client-Komponenten nutzen */}
+    <html lang="de" className="min-h-screen">
+      <body className="bg-gray-50 flex flex-col min-h-screen">
         <LanguageProvider>
           <FixedHeader />
-          <main className="pt-28 px-6">{children}</main>
+          
+          {/* Content-Bereich */}
+          <main className="pt-28 px-6 flex-1">
+            {children}
+          </main>
+
+          {/* Footer */}
+          <FixedFooter />
         </LanguageProvider>
       </body>
     </html>
