e25f4c5006036283a258a4576ba89056ddb77031
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-02-11 23:35:17 +0100

    Create ContentOrder.tsx

---
+++ src/components/ContentOrder.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import ModuleContent from './ModuleContent';
+import SupportContent from './SupportContent';
+
+export default function ContentOrder() {
+  return (
+    <>
+      <ModuleContent />
+      <SupportContent />
+    </>
+  );
+}
