771de2b6524d2c6d25255202a7eb7b73fb8b34f5
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-02-12 00:13:19 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from 'react';
 import FixedHeader from '../components/FixedHeader';
 import FixedFooter from '../components/FixedFooter';
 import { LanguageProvider } from '../context/LanguageContext';
+import Background from '../components/Background';
 
 export const metadata = {
   title: 'AstroPlays Discord Bot',
@@ -12,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="de" className="min-h-screen">
-      <body className="bg-gray-50 flex flex-col min-h-screen">
+      <body className="bg-neutral-950 flex flex-col min-h-screen text-white">
+        {/* GLOBALER HINTERGRUND */}
+        <Background />
+
         <LanguageProvider>
           <FixedHeader />
 
