8a9a60d3320cb0304c6a6cdc15dfc608eccbfaa0
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-02-21 00:28:36 +0100

    Delete src/lib directory

--- src/lib/discordApi.ts
+++
@@ -1,9 +0,0 @@
-// lib/discordApi.ts
-export async function fetchDiscordMember(userId: string, accessToken: string, guildId: string) {
-  const res = await fetch(`https://discord.com/api/users/@me/guilds/${guildId}/member`, {
-    headers: { Authorization: `Bearer ${accessToken}` },
-  });
-
-  if (!res.ok) return null;
-  return await res.json(); // { roles: [...] }
-}
