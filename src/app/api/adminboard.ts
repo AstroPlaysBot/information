a77f843c185377da073623d922edb37bdcb4b758
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-02-22 14:41:16 +0100

    Update adminboard.ts

--- src/app/api/adminboard.ts
+++ src/app/api/adminboard.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-//import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-//const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') return res.status(405).end();
