c31118f754b038bc3e5354d972b0fa7667b5280a
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-02-26 23:50:19 +0100

    Update route.ts

--- src/app/api/guild-users/[guildId]/route.ts
+++ src/app/api/guild-users/[guildId]/route.ts
@@ -1,6 +1,6 @@
 // src/app/api/guild-users/[guildId]/route.ts
 import { NextResponse } from 'next/server';
-import prisma from '@/lib/prisma';
+import { prisma } from '@/lib/prisma';
 
 export async function GET(req: Request, { params }: { params: { guildId: string } }) {
   const { guildId } = params;
