d3cc484a2bc713e824c62f97d1ddfc4fa899ce90
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-02-27 20:47:53 +0100

    Delete src/secure directory

--- src/secure/session.ts
+++
@@ -1,7 +0,0 @@
-// src/secure/session.ts
-import { cookies } from 'next/headers';
-
-export function getTokenCookie(cookieName: 'personal_token' | 'user_token' = 'personal_token') {
-  const token = cookies().get(cookieName)?.value;
-  return token || null;
-}
