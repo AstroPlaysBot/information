130647ad7631bbe39680c0f0a71163d422468f3e
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-03-01 23:00:23 +0100

    Update page.tsx

--- src/app/AGB/page.tsx
+++ src/app/AGB/page.tsx
@@ -1,17 +1,17 @@
 // src/app/AGB/page.tsx
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from './layout';
 import { agb } from './texts';
 
 export default function AGBPage() {
+  const [language, setLanguage] = useState<'de' | 'en'>('de');
+
   return (
     <Layout>
-      {({ language }) => (
-        <div className="prose prose-invert max-w-3xl">
-          <div dangerouslySetInnerHTML={{ __html: agb[language].replace(/\n/g, '<br/>') }} />
-        </div>
-      )}
+      <div className="prose prose-invert max-w-3xl">
+        <div dangerouslySetInnerHTML={{ __html: agb[language].replace(/\n/g, '<br/>') }} />
+      </div>
     </Layout>
   );
 }
