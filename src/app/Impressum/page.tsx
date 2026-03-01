b986cc4fce6a8daf043c5ca11a5aa59b9164c067
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-03-01 22:45:01 +0100

    Create page.tsx

---
+++ src/app/Impressum/page.tsx
@@ -0,0 +1,17 @@
+// src/app/Impressum/page.tsx
+'use client';
+import React from 'react';
+import Layout from './layout';
+import { agb } from './impressum';
+
+export default function ImpreesumPage() {
+  return (
+    <Layout>
+      {({ language }) => (
+        <div className="prose prose-invert max-w-3xl">
+          <div dangerouslySetInnerHTML={{ __html: agb[language].replace(/\n/g, '<br/>') }} />
+        </div>
+      )}
+    </Layout>
+  );
+}
