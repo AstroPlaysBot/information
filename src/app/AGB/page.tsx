8ef145fa5cf50e08bbfc90648acfb07584500c5b
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-03-01 22:42:35 +0100

    Create page.tsx

---
+++ src/app/AGB/page.tsx
@@ -0,0 +1,17 @@
+// src/app/AGB/page.tsx
+'use client';
+import React from 'react';
+import Layout from './layout';
+import { agb } from './texts';
+
+export default function AGBPage() {
+  return (
+    <Layout>
+      {({ language }) => (
+        <div className="prose prose-invert max-w-3xl">
+          <div dangerouslySetInnerHTML={{ __html: agb[language].replace(/\n/g, '<br/>') }} />
+        </div>
+      )}
+    </Layout>
+  );
+}
