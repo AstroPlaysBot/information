e903bfff394d4c97852dc1584e6ccdc321cb71fe
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-03-02 21:56:05 +0100

    Update route.ts

--- src/app/api/guilds/route.ts
+++ src/app/api/guilds/route.ts
@@ -20,7 +20,7 @@ interface DiscordUser {
   avatar?: string;
 }
 
-type RoleType = 'OWNER' | 'CO_OWNER' | 'TEILHABER';
+type RoleType = 'OWNER' | 'CO_OWNER' | 'PARTNER';
 
 function isDiscordGuild(obj: any): obj is DiscordGuild {
   return obj && typeof obj.id === 'string' && typeof obj.name === 'string' && typeof obj.owner === 'boolean';
