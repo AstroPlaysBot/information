fd4f88845bdf4871c7733a794610cdfc9b92aec4
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-03-20 23:15:40 +0100

    Update page.tsx

--- src/app/adminboard/page.tsx
+++ src/app/adminboard/page.tsx
@@ -1,32 +1,13 @@
 // src/app/adminboard/page.tsx
-'use client'
-
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-import { useState, useEffect } from 'react'
-import AdminAnimation from './AdminAnimation'
-import AdminBoardWrapper from './AdminBoardWrapper'
+import AdminBoardClientLoader from './AdminBoardClientLoader'
 
 export const dynamic = 'force-dynamic'
 
 export default function AdminBoardPage() {
-  const cookieStore = cookies()
-  const adminToken = cookieStore.get('admin_token')
+  const adminToken = cookies().get('admin_token')
+  if (!adminToken) redirect('/')
 
-  if (!adminToken) {
-    redirect('/')
-  }
-
-  // Steuerung, ob Animation oder Board gezeigt wird
-  const [showAnimation, setShowAnimation] = useState(true)
-
-  return (
-    <>
-      {showAnimation ? (
-        <AdminAnimation onFinish={() => setShowAnimation(false)} />
-      ) : (
-        <AdminBoardWrapper />
-      )}
-    </>
-  )
+  return <AdminBoardClientLoader />
 }
